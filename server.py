041c83092c3c0df665131877838dab76fb32067c
John Gunderman <johngunderman@gmail.com>
2012-05-27 15:38:36 -0700

    Add header menu to main page

--- server.py
+++ server.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+@app.route('/new')
+def hello_world():
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run()
