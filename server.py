711525aadf7b44acec2aea93f61054883233b047
John Gunderman <johngunderman@gmail.com>
2012-05-27 15:46:10 -0700

    Reuse stories.html for /top and /new

--- server.py
+++ server.py
@@ -3,12 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('stories.html')
+def top_stories():
+    return render_template('stories.html', page_name="Top Stories")
 
 @app.route('/new')
-def hello_world():
-    return render_template('stories.html')
+def new_stories():
+    return render_template('stories.html', page_name="New Stories")
 
 
 if __name__ == '__main__':
